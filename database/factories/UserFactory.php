14ec2cd82062a41d96fd57e64b23fae83e722205
keniaalves <kap-araujo@outlook.com>
2018-06-12 00:48:02 -0300

    Correções

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,24 +22,3 @@ $factory->define(App\User::class, function (Faker $faker) {
     ];
 });
 
-$factory->define(App\Thread::class, function ($faker) {
-    return[
-        'user_id'=> function () {
-            return factory('App\User')->create()->id;
-        },
-        'title' => $faker->sentence,
-        'body'  => $faker->paragraph
-    ];
-});
-
-$factory->define(App\Reply::class, function ($faker) {
-    return[
-        'user_id'=> function () {
-            return factory('App\User')->create()->id;
-        },
-        'thread_id'=> function () {
-            return factory('App\Thread')->create()->id;
-        },
-        'body'  => $faker->paragraph
-    ];
-});
