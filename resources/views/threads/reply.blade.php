210ca6926fbb698cf29bba8b76ad810130321102
Kenia <kap-araujo@outlook.com>
2018-07-18 08:08:55 -0300

    Aula das queues e alteração de migration.

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -2,11 +2,10 @@
 <div class="alert alert-default">
     {{ $reply->created_at->diffForHumans() }} |
     <a href="#">{{$reply->owner->name}}</a>
-    {{ $reply->favorites->count() }}
     <form method="POST" action="/replies/{{ $reply->id }}/favorites">
     {{ csrf_field() }}
         <button type="submit" class="btn btn-success" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-        {{ $reply->favorites->count() }} {{ str_plural ('Favorite', $reply->favorites->count() )}} 
+        {{ $reply->favorites_count }} {{ str_plural ('Favorite', $reply->favorites_count )}} 
         
         </button>
     </form>
